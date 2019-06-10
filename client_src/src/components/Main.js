63cdd5e890302b9ddcc6e974d1a91fac590913fd
Kevin <stradtkt22@gmail.com>
2019-06-10 11:50:52 -0400

    Added post a meetup

--- client_src/src/components/Main.js
+++ client_src/src/components/Main.js
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router-dom';
 import Meetups from './Meetups';
 import About from './About';
 import MeetupDetails from './MeetupDetails';
+import AddMeetup from './AddMeetup';
 
 
 const Main = () => (
     <Switch>
         <Route exact={true} path='/' component={Meetups}/>
         <Route exact={true} path='/about' component={About}/>
+        <Route exact={true} path='/meetups/add' component={AddMeetup}/>
         <Route exact={true} path='/meetups/:id' component={MeetupDetails}/>
     </Switch>
 );
